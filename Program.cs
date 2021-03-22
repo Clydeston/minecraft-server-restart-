2b71dae89bb2ced9827f6fc380c29fee1aa926d0
Clydeston <Clydeston@users.noreply.github.com>
2021-03-22 12:07:44 +0000

    added json config
    big improvements

--- Program.cs
+++ Program.cs
@@ -16,69 +16,78 @@ namespace SendKeys
     {
         [DllImport("User32.dll")]
         static extern int SetForegroundWindow(IntPtr p);
-        static IntPtr hWnd = (IntPtr)0;
+        static IntPtr hWnd = (IntPtr)0;        
+        static int[] intervals  = new int[] { 5000, 5000, 5000 };
+
+        static string process_name = "";
+        static string server_launch_script = "";
+        static int proc_id = 0;
 
         static void Main(string[] args)
         {
             Process[] processes = Process.GetProcesses();
             if(processes != null)
             {
-                foreach(Process p in processes)
+                using (StreamReader r = new StreamReader("./config.json"))
+                {
+                    dynamic json = JsonConvert.DeserializeObject(r.ReadToEnd());
+                    process_name = json["process_name"];
+                    server_launch_script = json["server_launch"];
+                }
+
+                foreach (Process p in processes)
                 {                    
-                    if (p.MainWindowTitle.Contains("Minecraft"))
-                    {
+                    if (p.MainWindowTitle.Contains(process_name))
+                    {                           
+                        proc_id = p.Id;
                         hWnd = p.MainWindowHandle;
+
                         SetForegroundWindow(hWnd);
                         System.Windows.Forms.SendKeys.SendWait("say Server will restart in two minutes!");
                         System.Windows.Forms.SendKeys.SendWait("{ENTER}");
                         System.Timers.Timer timer = new System.Timers.Timer();
                         timer.Elapsed += new ElapsedEventHandler(FirstRestartNotification);
-                        timer.Interval = 90000;
+                        timer.Interval = intervals[0];
                         timer.Enabled = true;
-                        timer.AutoReset = false;                        
+                        timer.AutoReset = false;
+                        break;                                               
                     }
                 }
                 Console.ReadLine();
-            }            
+            }
+            Environment.Exit(0);
         }
 
         private static void FirstRestartNotification(object source, ElapsedEventArgs e)
-        {
-            SetForegroundWindow(hWnd);
+        {            
             System.Windows.Forms.SendKeys.SendWait("say Server will restart in 30 seconds!");
             System.Windows.Forms.SendKeys.SendWait("{ENTER}");
             System.Timers.Timer timer = new System.Timers.Timer();
             timer.Elapsed += new ElapsedEventHandler(Restart);
-            timer.Interval = 30000;
+            timer.Interval = intervals[1];
             timer.Enabled = true;
             timer.AutoReset = false;
         }
 
         private static void Restart(object source, ElapsedEventArgs e)
-        {
-            SetForegroundWindow(hWnd);
+        {            
             System.Windows.Forms.SendKeys.SendWait("say Server stopping!");
             System.Windows.Forms.SendKeys.SendWait("{ENTER}");
             System.Windows.Forms.SendKeys.SendWait("stop");
             System.Windows.Forms.SendKeys.SendWait("{ENTER}");
-            System.Timers.Timer timer = new System.Timers.Timer();
-            timer.Elapsed += new ElapsedEventHandler(ExitServerConsole);
-            timer.Interval = 6000;
-            timer.Enabled = true;
-            timer.AutoReset = false;            
+            ExitServerConsole();       
         }
 
-        private static void ExitServerConsole(object source, ElapsedEventArgs e)
+        private static void ExitServerConsole()
         {
-            string path = @"C:\Users\Administrator\Desktop\Minecraft Servers\the-1122-pack_1.3.2\LaunchServer.bat";
+            Process process = Process.GetProcessById(proc_id);
+            process.Kill();
+
+            string path = server_launch_script;
             ProcessStartInfo psi = new ProcessStartInfo();
             psi.WorkingDirectory = Path.GetDirectoryName(path);
             psi.FileName = Path.GetFileName(path);
             Process.Start(psi);
-
-            SetForegroundWindow(hWnd);            
-            System.Windows.Forms.SendKeys.SendWait("{ENTER}");
-            System.Windows.Forms.SendKeys.SendWait("{ENTER}");            
             Environment.Exit(0);
         }
     }
